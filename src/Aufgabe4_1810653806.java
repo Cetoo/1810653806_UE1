780decad51fdd103d776a5f86a9f9e271eeac1ba
Cetoo <1810653806@stud.fh-kufstein.ac.at>
2018-11-19 14:21:59 +0100

    Aufgabe4

---
+++ src/Aufgabe4_1810653806.java
@@ -0,0 +1,29 @@
+public class Aufgabe4_1810653806
+{
+
+    public static void main(String[] args)
+    {
+
+        int Zahl1=7;
+        int Zahl2=3;
+
+        System.out.println(Zahl1<Zahl2);
+        System.out.println(Zahl2<Zahl1);
+        System.out.println(Zahl1>Zahl2);
+        System.out.println(Zahl2>Zahl1);
+        System.out.println(Zahl1>=Zahl2);
+        System.out.println(Zahl2<=Zahl1);
+        System.out.println(Zahl1>=Zahl2);
+        System.out.println(Zahl2>=Zahl1);
+        System.out.println(Zahl1==Zahl2);
+        System.out.println(Zahl1!=Zahl2);
+
+        /*Ergebnis wird mit true oder false angegeben*/
+    }
+
+
+
+
+
+
+}

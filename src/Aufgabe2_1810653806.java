334ed439184b8068410b58a6a5b839bb33fbfc93
Cetoo <1810653806@stud.fh-kufstein.ac.at>
2018-11-19 13:54:45 +0100

    Uebung2

---
+++ src/Aufgabe2_1810653806.java
@@ -0,0 +1,34 @@
+public class Aufgabe2_1810653806
+{
+    public static void main(String[] args)
+    {
+        String Name="Dennis";    /*Mein Wert ist Dennis und mein Bereich ist innerhalb der "..."*/
+
+        int Alter=25;           /*Mein Wert ist hier eine Zahl bzw. mein Alter und mein Wertebereich liegt zwischen
+                                   -2147483648 und 2147483648 (32byte)*/
+
+        double Kommastelle=7.7; /*Mein Wert ist eine Fließkommawert und mein Wertebereich liegt laut IEEE 754 zwischen
+                                  circa 4.9E-324 und 1.8E308 (Doppelte Genauigkeit) (64 Byte)*/
+
+        boolean Wahrheitswert=true; /*Hier wird ein Wahrheitswert berechnet in dem Bereich true oder false*/
+
+        long GanzeZahlen=64973748; /*Gibt mir ganze Zahlen aus zwischen -9223372036854775808
+                                     und 9223372036854775807 (64 Bit)*/
+
+        char einzelneZeichen='\uffff';  /*Gibt mir einzelne Zeichen eines Unicodes an zwischen '\u0000' und '\uffff' (16 Bit)*/
+
+        byte ganzeZahlen1= -125;    /*Gibt mir ebenfalls ganze Zahlen an aber nur zwischen -128 und 127 (8 Bit)*/
+
+        short ganzeZahlen2=-32767;        /*Gibt mir ebenfalls ganze Zahlen an aber nur zwischen -32768 und 32767*/
+
+        float Fliesskommawert=25.9898544874494715f; /*Gibt eine Kommazahl nach IEEE 754 zwischen ca. 1.4E-45
+                                                    und 3.4E38 (Einfache Genauigkeit, 32 Bit)*/
+
+        final int zahl=218;         /*Der Befehl final wandelt die Variable in eine Konstante um*/
+
+
+
+    }
+
+
+}

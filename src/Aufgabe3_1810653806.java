84dfb7d96696cee05451f23c87bfcc20444fd8ab
Cetoo <1810653806@stud.fh-kufstein.ac.at>
2018-11-19 14:11:31 +0100

    Aufgabe3

---
+++ src/Aufgabe3_1810653806.java
@@ -0,0 +1,22 @@
+public class Aufgabe3_1810653806
+{
+    public static void main(String[] args)
+    {
+        int Zahl1=25;
+        int Zahl2=25;
+        int Zahl3=1;
+
+        double Zahl4=1.5;
+        double Zahl5=2.5;
+        double Zahl6=3.5;
+
+        System.out.println(Zahl1+Zahl2);    /*Addieren*/
+        System.out.println(Zahl1/Zahl2);    /*Dividieren*/
+        System.out.println(Zahl4*Zahl2);    /*Multiplizieren*/
+        System.out.println(Zahl1%Zahl2);    /*Prozentrechnen*/
+        System.out.println(++Zahl3);        /*Zahl erhöht sich sofort um 1*/
+        System.out.println(--Zahl2);        /*Zahl verringert sich sofort um 1*/
+        System.out.println("_____________________________________");    /*Zeile zum draufschreiben*/
+
+    }
+}

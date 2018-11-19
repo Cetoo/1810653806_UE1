f629cc843f3935a927b4aa9b0c31ffa39c760da4
Cetoo <1810653806@stud.fh-kufstein.ac.at>
2018-11-19 14:31:57 +0100

    Aufgabe5

--- src/Aufgabe4_1810653806.java
+++ src/Aufgabe4_1810653806.java
@@ -22,8 +22,4 @@ public class Aufgabe4_1810653806
     }
 
 
-
-
-
-
 }
---
+++ src/Aufgabe5_1810653806.java
@@ -0,0 +1,20 @@
+public class Aufgabe5_1810653806
+{
+    public static void main(String[] args)
+    {
+        int Zahl1=5;
+        int Zahl2=38;
+
+        System.out.println(Zahl1++);
+        System.out.println(Zahl2++);
+        System.out.println(Zahl1);
+        System.out.println(Zahl2);
+        System.out.println(++Zahl1);
+        System.out.println(++Zahl2);
+
+        /*Bei dem Befehl Zahl++ wird meine Zahl zwar um 1 jeweils erhöht mir wird dies aber nicht sofort in der Konsole
+         angezeigt sondern erst wenn ich die jeweilige Zahl nachträglich noch einmal in meiner Konsole ausgebe sehe ich das Ergebnis.
+         Der Befehl ++Zahl gibt mir ein sofortiges Ergebnis auf der Konsole aus und verwendet dafür allerdings die schon einmal
+         um eins erhöhte Zahl*/
+    }
+}
